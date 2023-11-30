7ba884e601eb395dbc9017ddaf273ac56ff29d23
Tech챕터/김현기 <grissom@infinistar.co.kr>
2023-11-30 17:39:23 +0900

    survey

--- app/src/main/java/kr/co/eclipse4j/MainActivity.kt
+++ app/src/main/java/kr/co/eclipse4j/MainActivity.kt
@@ -1,13 +1,20 @@
 package kr.co.eclipse4j
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Html
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -28,11 +35,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import io.ktor.client.call.body
 import io.ktor.client.request.get
-import io.ktor.http.cio.Response
 import kr.co.eclipse4j.client.KtorClient
 import kr.co.eclipse4j.client.response.Survey
 import kr.co.eclipse4j.client.response.SurveyList
@@ -55,6 +64,7 @@ class MainActivity : ComponentActivity() {
 }
 
 /** List of Items */
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SurveyListView(
     modifier: Modifier = Modifier
@@ -66,9 +76,29 @@ fun SurveyListView(
     }
 
     LazyColumn(modifier=modifier.padding(vertical = 4.dp)){
-        items(surveys) {survey ->
-            SurveyList(survey = survey)
+        stickyHeader {
+            Header()
         }
+        items(surveys) {survey ->
+            SurveyList(LocalContext.current, survey = survey)
+        }
+    }
+}
+
+@Composable
+fun Header(){
+    Column(
+        modifier = Modifier
+            .height(40.dp)
+            .fillMaxWidth()
+            .background(Color.LightGray)
+    ) {
+        Text(
+            text = "설문리스트",
+            modifier = Modifier.fillMaxWidth(),
+            style = MaterialTheme.typography.headlineLarge,
+            textAlign = TextAlign.Center
+        )
     }
 }
 
@@ -80,16 +110,22 @@ suspend fun getApiData(): SurveyList {
 
 /** UI for Card */
 @Composable
-fun SurveyList(survey: Survey) {
-
+fun SurveyList(context: Context, survey: Survey) {
     Card(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.primary
         ),
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp)
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 4.dp)
+            .clickable { cardViewCallBack(context) }
     ) {
         SurveyContent(survey = survey)
     }
+}
+
+fun cardViewCallBack(context: Context){
+    val myToast = Toast.makeText(context, "Click!!!", Toast.LENGTH_SHORT)
+    myToast.show()
 }
 
 /** UI for Card Content*/
